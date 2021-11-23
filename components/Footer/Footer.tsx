d6bf0f4f78181fa7409d1046d46a7296823352ca
mark22312 <mark22312@gmail.com>
2021-11-24 00:48:36 +0200

    Done footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,6 @@
+import { faFacebook, faInstagram, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from 'react';
 import { BASE_IMAGE_ICON } from '../../config/images';
 
@@ -16,7 +19,32 @@ export const Footer: React.FC = () => {
 				<p>קולקטיב רדיו אינטרנטי</p>
 				</div>
 			</div>
-		<div>links</div>
-		<div>akum</div></footer>
+		<div className={styles.socialsWrapper}>
+			<div className={styles.socialIconsWrapper}>
+				<SocialIcon url={'mailto:radiosavta@gmail.com'} icon={faEnvelope}/>
+				<SocialIcon url={'https://www.instagram.com/radiosavta"'} icon={faInstagram}/>
+				<SocialIcon url={'https://www.facebook.com/radiosavta'} icon={faFacebook}/>
+			</div>
+			<p className={styles.socialsText}>כתבו לנו | עקבו אחרינו  | שתפו אותנו</p>
+		</div>
+		<div>
+				<img src={`${BASE_IMAGE_ICON}radiosavta/assets/white_acum`} className={styles.acumLogo} alt={'אקו"ם'}/>
+		</div></footer>
 	)
+}
+
+const SocialIcon: React.FC<{
+	url: string;
+	icon: IconDefinition;
+}> = (props) => {
+return (
+	<a
+            className={styles.socialIcon}
+            target="_blank"
+            href={props.url}
+			rel="noreferrer noopener"
+          >
+            <FontAwesomeIcon icon={props.icon} size="1x" color="white" />
+          </a>
+)
 }
