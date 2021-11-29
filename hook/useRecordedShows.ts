7776836bf13d673aeb7f8321cbb5ef1a9240b3f3
mark22312 <mark22312@gmail.com>
2021-11-29 09:24:29 +0200

    Format

--- hook/useRecordedShows.ts
+++ hook/useRecordedShows.ts
@@ -1,57 +1,53 @@
+import { QueryClient, useInfiniteQuery } from "react-query";
+import { queryRecordedShows } from "../api/RecordedShows.api";
 
+interface RecordedShowsFilter {
+  search?: string;
+  programId?: number;
+  limit?: number;
+}
+export const prefetchRecordedShows = (
+  queryClient: QueryClient,
+  args?: RecordedShowsFilter
+) => {
+  return queryClient.prefetchInfiniteQuery(
+    `recordedShows-archive${args?.search}${args?.programId}`,
+    ({ pageParam = 1 }) =>
+      queryRecordedShows({
+        page: pageParam,
+        search: args?.search || undefined,
+        programId: args?.programId,
+        limit: args?.limit || undefined,
+      })
+  );
+};
 
-  import { useState } from "react";
-  import { QueryClient, useInfiniteQuery } from "react-query";
-  import { queryRecordedShows } from "../api/RecordedShows.api";
-  
-  interface RecordedShowsFilter {
-	search?: string;
-	programId?: number;
-	limit?: number;
-  }
-  export const prefetchRecordedShows = (
-	queryClient: QueryClient,
-	args?: RecordedShowsFilter
-  ) => {
-	return queryClient.prefetchInfiniteQuery(
-	  `recordedShows-archive${args?.search}${args?.programId}`,
-	  ({ pageParam = 1 }) =>
-		queryRecordedShows({
-		  page: pageParam,
-		  search: args?.search || undefined,
-		  programId: args?.programId,
-		  limit: args?.limit || undefined,
-		})
-	);
+export const useRecordedShows = (args?: RecordedShowsFilter) => {
+  const queryData = useInfiniteQuery(
+    `recordedShows-archive${args?.search}${args?.programId}`,
+    // someCode
+    ({ pageParam = 1 }) =>
+      queryRecordedShows({
+        page: pageParam,
+        search: args?.search || undefined,
+        programId: args?.programId,
+        limit: args?.limit || undefined,
+      }),
+    {
+      getNextPageParam: (lastPage) => lastPage.data.pageData.next.page,
+      refetchOnWindowFocus: false,
+    }
+  );
+  const { data, isLoading, hasNextPage, fetchNextPage } = queryData;
+
+  const fetchNext = () => {
+    fetchNextPage();
   };
-  
-  export const useRecordedShows = (args?: RecordedShowsFilter) => {
-	const queryData = useInfiniteQuery(
-	  `recordedShows-archive${args?.search}${args?.programId}`,
-	  // someCode
-	  ({ pageParam = 1 }) =>
-		queryRecordedShows({
-		  page: pageParam,
-		  search: args?.search || undefined,
-		  programId: args?.programId,
-		  limit: args?.limit || undefined,
-		}),
-	  {
-		getNextPageParam: (lastPage) => lastPage.data.pageData.next.page,
-		refetchOnWindowFocus: false,
-	  }
-	);
-	const { data, isLoading, hasNextPage, fetchNextPage } = queryData;
-  
-	const fetchNext = () => {
-	  fetchNextPage();
-	};
-  
-	return {
-	  recordedShows: data?.pages.map((p) => p.data.recordedShows),
-	  isLoading,
-	  fetchNext,
-	  hasNextPage,
-	};
+
+  return {
+    recordedShows: data?.pages.map((p) => p.data.recordedShows),
+    isLoading,
+    fetchNext,
+    hasNextPage,
   };
-  
+};
