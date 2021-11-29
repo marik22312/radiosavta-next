a941883524069610e3cf11a2897eacbcd6aeb2fc
mark22312 <mark22312@gmail.com>
2021-11-29 09:23:48 +0200

    Prefetch recordedShows

--- hook/useRecordedShows.ts
+++ hook/useRecordedShows.ts
@@ -1,27 +1,57 @@
-import { useState } from "react";
-import { useInfiniteQuery } from "react-query";
-import { queryRecordedShows } from "../api/RecordedShows.api";
 
-export const useRecordedShows = (args?: {search?: string, programId?: number, limit?: number}) => {
-  const queryData = useInfiniteQuery(
-    `recordedShows-archive${args?.search}${args?.programId}`,
-	// someCode
-    ({ pageParam = 1 }) => queryRecordedShows({ page: pageParam, search: args?.search || undefined, programId: args?.programId, limit: args?.limit || undefined }),
-    {
-      getNextPageParam: (lastPage) => lastPage.data.pageData.next.page,
-      refetchOnWindowFocus: false,
-    }
-  );
-  const {data, isLoading, hasNextPage, fetchNextPage} = queryData;
+
+  import { useState } from "react";
+  import { QueryClient, useInfiniteQuery } from "react-query";
+  import { queryRecordedShows } from "../api/RecordedShows.api";
   
-  const fetchNext = () => {
-    fetchNextPage();
+  interface RecordedShowsFilter {
+	search?: string;
+	programId?: number;
+	limit?: number;
+  }
+  export const prefetchRecordedShows = (
+	queryClient: QueryClient,
+	args?: RecordedShowsFilter
+  ) => {
+	return queryClient.prefetchInfiniteQuery(
+	  `recordedShows-archive${args?.search}${args?.programId}`,
+	  ({ pageParam = 1 }) =>
+		queryRecordedShows({
+		  page: pageParam,
+		  search: args?.search || undefined,
+		  programId: args?.programId,
+		  limit: args?.limit || undefined,
+		})
+	);
   };
-
-  return {
-    recordedShows: data?.pages.map((p) => p.data.recordedShows),
-    isLoading,
-    fetchNext,
-    hasNextPage,
+  
+  export const useRecordedShows = (args?: RecordedShowsFilter) => {
+	const queryData = useInfiniteQuery(
+	  `recordedShows-archive${args?.search}${args?.programId}`,
+	  // someCode
+	  ({ pageParam = 1 }) =>
+		queryRecordedShows({
+		  page: pageParam,
+		  search: args?.search || undefined,
+		  programId: args?.programId,
+		  limit: args?.limit || undefined,
+		}),
+	  {
+		getNextPageParam: (lastPage) => lastPage.data.pageData.next.page,
+		refetchOnWindowFocus: false,
+	  }
+	);
+	const { data, isLoading, hasNextPage, fetchNextPage } = queryData;
+  
+	const fetchNext = () => {
+	  fetchNextPage();
+	};
+  
+	return {
+	  recordedShows: data?.pages.map((p) => p.data.recordedShows),
+	  isLoading,
+	  fetchNext,
+	  hasNextPage,
+	};
   };
-};
+  
