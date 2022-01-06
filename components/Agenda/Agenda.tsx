ede3271333c6aa33cb4783222f6c4df3ec4558df
Asaf Hadad <asaf147369@gmail.com>
2022-01-06 20:57:21 +0200

    changes to desktop agenda and sort by hour

--- components/Agenda/Agenda.tsx
+++ components/Agenda/Agenda.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Schedule } from "../../domain/Schedule";
 import { useAgenda } from "../../hook/useAgenda";
 import styles from './Agenda.module.scss';
@@ -7,9 +7,20 @@ export const Agenda: React.FC = () => {
 
     const [isOpen, setIsOpen] = useState<Boolean>(false)
 
+    const [schedule, setSchedule] = useState<Schedule[]>([])
+
     const agendaClassName = isOpen ? styles.open : styles.closed;
 
     const { data } = useAgenda();
+
+    useEffect(() => {
+        if(data?.schedule) {
+            const sorted = data?.schedule.sort((a,b) => {
+                return new Date(a.start).getHours() - new Date(b.start).getHours();
+              })
+              setSchedule(sorted)
+        }
+    }, [data])
 
     const timeOptions: DateTimeFormatOptions = {
         hourCycle: 'h23',
@@ -21,7 +32,10 @@ export const Agenda: React.FC = () => {
         <div className={`${styles.agendaWrapper} ${agendaClassName}`}>
             <button className={styles.agendaButton} onClick={() => setIsOpen(!isOpen)} ><span className={styles.toggleIcon}></span></button>
             <div className={styles.agenda}>
-                {data?.schedule.map((e: Schedule) => {
+                <h2 className={styles.agendaTitle}>
+                    מה היום?
+                </h2>
+                {schedule.map((e: Schedule) => {
                     return (
                     <div className={styles.agendaProgram} key={e.id}>
                         <span className={styles.programName}>{e.name}</span> - <span className={styles.programTime}>{new Intl.DateTimeFormat('en-US', timeOptions).format(new Date(e.start))}</span>
