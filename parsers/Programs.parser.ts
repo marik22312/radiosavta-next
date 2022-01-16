0c2ec4996d43ab8b8a339b63531f32e4fc57fdad
Marik Shnitman <mark22312@gmail.com>
2022-01-17 00:53:40 +0200

    Added program parser (#37)
    Co-authored-by: mark22312 <mark22312@gmail.com>
    Co-authored-by: Asaf Hadad <asaf147369@gmail.com>

---
+++ parsers/Programs.parser.ts
@@ -0,0 +1,14 @@
+import { BASE_IMAGE_ICON } from '../config/images';
+import { Program } from '../domain/Program';
+
+class ProgramParser {
+	public programImage(program: Program): string {
+		return `${BASE_IMAGE_ICON}/${program.cover_image || program.users[0].profile_image}`;
+	}
+
+	public name(program: Program): string {
+		return program.name_he || program.name_en
+	}
+}
+
+export const programParser = new ProgramParser();
