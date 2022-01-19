a3bf1f9aa967b85c174f5f682b22867ab340eb6b
Marik Shnitman <mark22312@gmail.com>
2022-01-19 18:03:47 +0200

    Feature/auto quality (#48)

--- components/Timeline/TimelineData.ts
+++ components/Timeline/TimelineData.ts
@@ -12,90 +12,90 @@ export const TimelineData:timelineItem[] = [
 		date: "קיץ 1979",
 		title: "",
 		text: "יעל ויצחק קרן עוברים למצפה רמון ומקימים בו את ביתם",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1606921614/radiosavta/gallery/watertower"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1606921614/radiosavta/gallery/watertower"
 	},
 	{
 		id: "1",
 		date: "2003 - 2005",
 		title: "",
 		text: "עירא מקים את ״רדיו ברסלב - מצפה רמון״ יחד עם רפאל חליבה, 94 אפ.אם דרך מגבר שהותאם לשידור ברדיוס של 3.6 ק״מ כדי לא לעורר רשויות ",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1633711103/radiosavta/assets/timeline/radioBreslev"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1633711103/radiosavta/assets/timeline/radioBreslev"
 	},
 	{
 		id: "2",
 		date: "2004",
 		title: "",
 		text: "ציפי נולדת, סבתא יעל גונבת אותה ואת כל המשפחה אליה הביתה. עירא וסבתא בקנוניה משותפת",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1633711103/radiosavta/assets/timeline/savta_take"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1633711103/radiosavta/assets/timeline/savta_take"
 	},
 	{
 		id: "3",
 		date: "2009",
 		title: "",
 		text: "סבתא יעל שומעת לראשונה את יעקב מילר",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/jacob_miller"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/jacob_miller"
 	},
 	{
 		id: "4",
 		date: "2010",
 		title: "",
 		text: "דיבורים על שידור רדיו אינטרנטי מהסלון של סבתא לא צולחים את הבעיות הטכניות",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1633711103/radiosavta/assets/timeline/technical"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1633711103/radiosavta/assets/timeline/technical"
 	},
 	{
 		id: "5",
 		date: "2012 - 2014",
 		title: "",
 		text: "המוזיקה לא מפסיקה לנגן (לא כולל שבת) - מסיבות רגאי בחמישי אצל יעל בסלון",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1633711103/radiosavta/assets/timeline/dontstop"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1633711103/radiosavta/assets/timeline/dontstop"
 	},
 	{
 		id: "6",
 		date: "ד׳ חשון תשע״ה  28.10.2014",
 		title: "סבתא יעל קרן עוזבת את המימד שלנו.",
 		text: "אמא לארבע בנות ואחד עשר נכדים ונכדות ילדי מצפה רמון. ת.נ.צ.ב.ה",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/savta_yael"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/savta_yael"
 	},
 	{
 		id: "7",
 		date: "אוקטובר 2014",
 		title: "החלטה להקים את החלום",
 		text: "נסיונות שידור רודי קרמרט ורובין גרונוול יקירי הרדיו מפצחים את טכניקה איך. שידורי נסיון דרך האתר listen to my radio",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1635617234/radiosavta/assets/timeline/rudishirarobin"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1635617234/radiosavta/assets/timeline/rudishirarobin"
 	},
 	{
 		id: "8",
 		date: "25.1.2014",
 		title: "",
 		text: "עולים לאוויר כפסקול לעילוי נשמתה וכניסוי אמנותי לראות מה ילד יום. מעבר לאתר radionomy",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1633711103/radiosavta/assets/timeline/newstation"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1633711103/radiosavta/assets/timeline/newstation"
 	},
 	{
 		id: "9",
 		date: "2015",
 		title: "עידן חדש",
 		text: "הקמת אולפן בהאנגר ״תוצרת מצפה״ תחילה בקומת הקרקע ואחכ בקומת הגלריה באולפן-סלון של התחנה",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/ira_xs"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/ira_xs"
 	},
 	{
 		id: "10",
 		date: "2016",
 		title: "",
 		text: "רדיו סבתא הופך לקולקטיב של שדרנים ושדרניות. שירה בן חמו תושבת העיירה באותו הזמן מפציעה בדברי ימי הרדיו והופכת אותו לתחנה קהילתית פעילה ומגוונת מאוד כולל גל ישראלי בשבתות אותו ערכה ותפעלה עד 2020 מריק שניטמן בן מצפה רמון מצטרף לתוכנית של יעל דיין ומאיה מאיר ונקשרת נפשו ברוח המקום. תוך זמן קצר הוא נהפך למנהל הטכני ואחראי התפעול של הרדיו",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/v1633711103/radiosavta/assets/timeline/newstation"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/v1633711103/radiosavta/assets/timeline/newstation"
 	},
 	{
 		id: "11",
 		date: "2016 - 2019",
 		title: "",
 		text: "רדיוסבתא משדר תוכניות ותכנים שונים ועורך ארועים פתוחים, הופעות, מסיבות, תיקלוטים ושיתופי פעולה שונים וגניבים",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/parties"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/c_thumb,w_500/v1635617470/radiosavta/assets/timeline/parties"
 	},
 	{
 		id: "12",
 		date: "2019",
 		title: "",
 		text: "סגירת האולפן ב׳תוצרת מצפה׳ בעקבות הקורונה וסיבות אישיות. שידורים נסיוניים מפאב 40 בטללים ואחריהן מעבר למודל אינטרנטי של רדיו מבוזר המשדר מכל כנפות הארץ ללא תחנה פיזית ומהווה בית נסיוני לשדרנים המבקשות במה וכן גם רדיו שמשדר פסקול מוזיקאלי משובח. בדוק.",
-		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto/c_thumb,w_500/v1635616719/radiosavta/assets/timeline/pub40_BW"
+		img: "https://res.cloudinary.com/marik-shnitman/image/upload/f_auto,dpr_auto,q_auto/c_thumb,w_500/v1635616719/radiosavta/assets/timeline/pub40_BW"
 	}
 ]
