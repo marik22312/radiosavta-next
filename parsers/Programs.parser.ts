1a1cc10a54cd5b7c3229251b97c263a36eab21d6
Marik Shnitman <mark22312@gmail.com>
2022-01-22 14:23:26 +0200

    Fallback program image to user profile pic (#52)

--- parsers/Programs.parser.ts
+++ parsers/Programs.parser.ts
@@ -3,8 +3,8 @@ import { Program } from '../domain/Program';
 
 class ProgramParser {
 	public programImage(program: Program): string {
-		return `${BASE_IMAGE_ICON}/${program.cover_image || program.users[0].profile_image}`;
-	}
+			return `${BASE_IMAGE_ICON}/${program.cover_image || program.users?.[0]?.profile_image}` || '';
+		}
 
 	public name(program: Program): string {
 		return program.name_he || program.name_en
