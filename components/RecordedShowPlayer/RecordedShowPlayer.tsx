3ef80a299644b0ac45e41e1e0e61c6184405e2b8
mark22312 <mark22312@gmail.com>
2022-02-12 18:54:16 +0200

    Added share modal

--- components/RecordedShowPlayer/RecordedShowPlayer.tsx
+++ components/RecordedShowPlayer/RecordedShowPlayer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./RecordedShowsPlayer.module.scss";
 
 import { usePlayerControls } from "../../hook/usePlayerControls";
@@ -6,10 +6,36 @@ import { usePlayerControls } from "../../hook/usePlayerControls";
 import { usePLayerState } from "../../hook/usePlayerState";
 import { PlayPauseButton } from "../PlayPauseButton/PlayPauseButton";
 import { logPlayRecordedShow } from "../../api/Mixpanel.api";
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import Modal from "react-modal";
+import {
+	EmailShareButton,
+	FacebookIcon,
+	FacebookMessengerShareButton,
+	FacebookShareButton,
+	HatenaShareButton,
+	InstapaperShareButton,
+	LineShareButton,
+	LinkedinShareButton,
+	LivejournalShareButton,
+	MailruShareButton,
+	OKShareButton,
+	PinterestShareButton,
+	PocketShareButton,
+	RedditShareButton,
+	TelegramShareButton,
+	TumblrShareButton,
+	TwitterShareButton,
+	ViberShareButton,
+	VKShareButton,
+	WhatsappIcon,
+	WhatsappShareButton,
+	WorkplaceShareButton
+  } from "react-share";
+import { Title } from '../Typography/Title';
 export interface RecordedShowPlayerProps {
   url: string;
   name: string;
@@ -17,7 +43,7 @@ export interface RecordedShowPlayerProps {
   recordingDate: string;
   backgroundImageUrl?: string;
   programId: number;
-  source: 'HOMEPAGE' | 'PROGRAM_PAGE' | 'ARCHIVE';
+  source: "HOMEPAGE" | "PROGRAM_PAGE" | "ARCHIVE";
   showId: string | number;
 }
 
@@ -26,6 +52,7 @@ export const RecordedShowPlayer: React.FC<RecordedShowPlayerProps> = (
 ) => {
   const { play, pause, resume } = usePlayerControls();
   const { isPlaying, title } = usePLayerState();
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
 
   const togglePlay = () => {
     if (title === props.name) {
@@ -43,8 +70,8 @@ export const RecordedShowPlayer: React.FC<RecordedShowPlayerProps> = (
     logPlayRecordedShow({
       programName: props.programName,
       showName: props.name,
-	  source: props.source,
-	  programId: props.programId,
+      source: props.source,
+      programId: props.programId,
     });
     return play({
       title: props.name,
@@ -54,24 +81,37 @@ export const RecordedShowPlayer: React.FC<RecordedShowPlayerProps> = (
   };
 
   const onShare = async () => {
-	  const url = new URL(window.location.href)
-	  const shareData = {
-		  url: `${url.origin}/archive?showId=${props.showId}`,
-		  text: `[${Intl.DateTimeFormat("he").format(new Date(props.recordingDate))}] ${props.programName} - ${props.name}`
+	  if (typeof window === 'undefined') {
+		  return null
 	  }
-	if(navigator?.canShare?.(shareData) && navigator.share) {
-		return navigator.share(shareData)
-	}
+    const url = new URL(window.location.href);
+    const shareData = {
+      url: `${url.origin}/archive?showId=${props.showId}`,
+      text: `[${Intl.DateTimeFormat("he").format(
+        new Date(props.recordingDate)
+      )}] ${props.programName} - ${props.name}`,
+    };
+    if (navigator?.canShare?.(shareData) && navigator.share) {
+      return navigator.share(shareData);
+    }
 
-	return alert('Can Share!') // TODO
-  }
-  
+    return setIsShareModalOpen(true);
+  };
+
   return (
-    <div
-      className={style.RecordedShowPlayer}
-    >
-      <div className={style.imageWrapper} style={{ backgroundImage: `url(${props.backgroundImageUrl})` }}>
-        {props.backgroundImageUrl && <Image src={props.backgroundImageUrl} layout="fill" className={style.programImg} alt="" />}
+    <div className={style.RecordedShowPlayer}>
+      <div
+        className={style.imageWrapper}
+        style={{ backgroundImage: `url(${props.backgroundImageUrl})` }}
+      >
+        {props.backgroundImageUrl && (
+          <Image
+            src={props.backgroundImageUrl}
+            layout="fill"
+            className={style.programImg}
+            alt=""
+          />
+        )}
       </div>
       <div className={style.titleWrapper}>
         <h5
@@ -90,9 +130,38 @@ export const RecordedShowPlayer: React.FC<RecordedShowPlayerProps> = (
           isPlaying={isPlaying && title === props.name}
         />
       </div>
-	  <button className={style.shareButtonWrapper} onClick={onShare}>
-        <FontAwesomeIcon icon={(faShareAlt as IconDefinition)} size="2x" color="white" />
+      <button className={style.shareButtonWrapper} onClick={onShare}>
+        <FontAwesomeIcon
+          icon={faShareAlt as IconDefinition}
+          size="2x"
+          color="white"
+        />
       </button>
+      <ShareModal onRequestClose={() => setIsShareModalOpen(false)} isOpen={isShareModalOpen} name={props.name} programName={props.programName} url={typeof window !== 'undefined' && `${new URL(window.location.href).origin}/archive?showId=${props.showId}`}/>
+    </div>
+  );
+};
+
+// Modal.setAppElement('#yourAppElement');
+const ShareModal: React.FC<any> = (props: any) => {
+  return (
+    <div>
+      <Modal
+	  className={style.shareModal}
+        isOpen={props.isOpen}
+        onAfterOpen={() => console.log("afterOpenModal")}
+        onRequestClose={props.onRequestClose}
+        contentLabel="Example Modal"
+      >
+		  <div className={style.shareModalContent}>
+        <h3 className={style.shareModalTitle}>שתפו את {props.name}</h3>
+		<div className={style.shareModalSocialWrapper}>
+			<FacebookShareButton url={props.url} quote="someShit"><FacebookIcon size={40}/></ FacebookShareButton>
+			<WhatsappShareButton url={props.url} title={`${props.programName} - ${props.name}`}><WhatsappIcon size={40}/></ WhatsappShareButton>
+		</div>
+        <input readOnly type="url" value={props.url} style={{width: '100%', height: '40px', fontSize: '1rem'}}/>
+		  </div>
+      </Modal>
     </div>
   );
 };
