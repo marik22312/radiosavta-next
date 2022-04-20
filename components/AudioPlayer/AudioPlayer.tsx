4d9ec8f080675b23a5a8a389a907248478067655
mark22312 <mark22312@gmail.com>
2022-04-20 17:20:46 +0300

    Refactor

--- components/AudioPlayer/AudioPlayer.tsx
+++ components/AudioPlayer/AudioPlayer.tsx
@@ -1,72 +1,15 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef} from "react";
 import { useLivePlayer } from '../../hook/useLivePlayer';
-import { PlayerState } from "../../providers/PlayerProvider/PlayerProviderV2";
-import { usePlayerControls } from "../../providers/PlayerProvider/usePlayerControls";
 import { usePlayerState } from "../../providers/PlayerProvider/usePlayerState";
 import { Seeker } from "./Seeker/Seeker";
+import { usePlayerBindings } from './usePlayerBinding';
 
 export const AduioPlayer: React.FC = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
-  const animationRef = useRef<any>();
-  const seekerRef = useRef<any>();
-  const [currentTime, setCurrentTime] = useState(0);
+  const {seekerRef, currentTime} = usePlayerBindings(audioRef);
 
-  const { playerState, audioUrl, setPlayerState, isPlaying, isPaused } = usePlayerState();
+  const { audioUrl, isPlaying, isPaused } = usePlayerState();
   const {isLive} = useLivePlayer()
-
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.addEventListener("canplay", () => {
-        setPlayerState(PlayerState.PLAYING);
-      });
-
-      audioRef.current.addEventListener("ended", () => {
-        setPlayerState(PlayerState.STOPPED);
-      });
-    }
-    return () => {
-      audioRef.current?.removeEventListener("canplay", () => null);
-      audioRef.current?.removeEventListener("ended", () => null);
-    };
-  }, [setPlayerState]);
-
-  useEffect(() => {
-    const whilePlaying = () => {
-      animationRef.current = requestAnimationFrame(whilePlaying);
-      if (seekerRef.current) {
-        (seekerRef.current as any).value = audioRef.current?.currentTime;
-        setCurrentTime(audioRef.current?.currentTime ?? 0);
-      }
-    };
-
-    const onPlay = () => {
-      audioRef.current?.play();
-      animationRef.current = requestAnimationFrame(whilePlaying);
-    };
-    const onPause = () => {
-      audioRef.current?.pause();
-      cancelAnimationFrame(animationRef.current as any);
-    };
-
-    if (audioRef.current && audioUrl) {
-      switch (playerState) {
-        case PlayerState.LOADING:
-          console.log("LoDINH");
-          break;
-
-        case PlayerState.PLAYING:
-          onPlay();
-          break;
-
-        case PlayerState.PAUSED:
-          onPause();
-          break;
-
-        default:
-          break;
-      }
-    }
-  }, [playerState, audioUrl]);
 
   const handlePlayerChange = (value: number) => {
     audioRef.current!.currentTime = value;
---
+++ components/AudioPlayer/usePlayerBinding.ts
@@ -0,0 +1,67 @@
+
+import {RefObject, useEffect, useRef, useState} from 'react';
+import { PlayerState } from '../../providers/PlayerProvider/PlayerProviderV2';
+import { usePlayerState } from '../../providers/PlayerProvider/usePlayerState';
+
+export const usePlayerBindings = (audioRef: RefObject<HTMLAudioElement>) => {
+	const animationRef = useRef<any>();
+	const seekerRef = useRef<any>();
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const { playerState, audioUrl, setPlayerState, isPlaying, isPaused } = usePlayerState();
+	useEffect(() => {
+		if (audioRef.current) {
+		  audioRef.current.addEventListener("canplay", () => {
+			setPlayerState(PlayerState.PLAYING);
+		  });
+	
+		  audioRef.current.addEventListener("ended", () => {
+			setPlayerState(PlayerState.STOPPED);
+		  });
+		}
+		return () => {
+		  audioRef.current?.removeEventListener("canplay", () => null);
+		  audioRef.current?.removeEventListener("ended", () => null);
+		};
+	  }, [setPlayerState]);
+	
+	  useEffect(() => {
+		const whilePlaying = () => {
+		  animationRef.current = requestAnimationFrame(whilePlaying);
+		  if (seekerRef.current) {
+			(seekerRef.current as any).value = audioRef.current?.currentTime;
+			setCurrentTime(audioRef.current?.currentTime ?? 0);
+		  }
+		};
+	
+		const onPlay = () => {
+		  audioRef.current?.play();
+		  animationRef.current = requestAnimationFrame(whilePlaying);
+		};
+		const onPause = () => {
+		  audioRef.current?.pause();
+		  cancelAnimationFrame(animationRef.current as any);
+		};
+	
+		if (audioRef.current && audioUrl) {
+		  switch (playerState) {
+			case PlayerState.LOADING:
+			  console.log("LoDINH");
+			  break;
+	
+			case PlayerState.PLAYING:
+			  onPlay();
+			  break;
+	
+			case PlayerState.PAUSED:
+			  onPause();
+			  break;
+	
+			default:
+			  break;
+		  }
+		}
+	  }, [playerState, audioUrl]);
+
+	  return {currentTime, seekerRef}
+}
