252b9d6aa27ac4da6eb54b1cb37b8d59b028fa00
Marik Shnitman <mark22312@gmail.com>
2023-03-03 23:18:49 +0200

    [Feature] Added contact us form & fixed scroll (#97)

--- pages/join-us/index.tsx
+++ pages/join-us/index.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Colors } from "../../components/ui/Colors";
 import { Page } from "../../components/ui/Page";
 import { Heading } from "../../components/ui/Typography";
+import ReCAPTCHA from "react-google-recaptcha";
 import style from "./joinUs.module.scss";
+import { ContactFormRequest } from "../../api/Contact.api";
+import { FormField } from "../../components/ui/FormField";
+import { useContactForm } from "../../hook/useContactForm";
+import { Alert, AlertType } from "../../components/ui/Alert";
+
+enum FormFields {
+  NAME = "fullname",
+  EMAIL = "email",
+  MESSAGE = "message",
+}
 
 const JoinUsPage: React.FC = () => {
+  const captchaRef = useRef<ReCAPTCHA>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { submitForm, isError, isSuccess } = useContactForm({
+    onSuccess: (data) => {
+      setIsLoading(false);
+      formRef.current?.reset();
+      captchaRef.current?.reset();
+    },
+    onError: (error) => {
+      captchaRef.current?.reset();
+      setIsLoading(false);
+    },
+  });
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const captcha = await captchaRef.current?.executeAsync();
+    const data: ContactFormRequest = {
+      // @ts-expect-error
+      [FormFields.NAME]: e.target[FormFields.NAME].value,
+      // @ts-expect-error
+      [FormFields.EMAIL]: e.target[FormFields.EMAIL].value,
+      // @ts-expect-error
+      [FormFields.MESSAGE]: e.target[FormFields.MESSAGE].value,
+      recaptcha: captcha ?? "",
+    };
+    submitForm(data);
+  };
   return (
     <Page>
       <div style={{ paddingRight: "21px" }}>
@@ -22,16 +65,79 @@ const JoinUsPage: React.FC = () => {
             לעזור לכםן להוציא את עצמכםן לאור יחד ומול קהל בייתי של חובבי רדיו
             כמותכםן
           </div>
-		  <div>
-			<p>ברוכים הבאות לסלון של סבתא!</p>
-		  </div>
-		  <div>
-			<p>דברו איתנו ב</p>
-		  </div>
+          <div>
+            <p>ברוכים הבאות לסלון של סבתא!</p>
+          </div>
         </div>
       </div>
+      <section>
+        <div style={{ paddingRight: "21px", marginTop: "35px" }}>
+          <Heading color={Colors.SAVTA_ORANGE}>דברו איתנו</Heading>
+        </div>
+        <div className={style.formWrapper}>
+          <form ref={formRef} onSubmit={onSubmit}>
+            <FormField
+              label="שם מלא"
+              name={FormFields.NAME}
+              placeholder="ישראל ישראלי"
+            />
+            <FormField
+              inputType="email"
+              label="אימייל"
+              name={FormFields.EMAIL}
+              placeholder="israel@israeli.co.il"
+            />
+            <FormField
+              label="הודעה"
+              type="textarea"
+              name={FormFields.MESSAGE}
+              placeholder="איזה אלופים אתם! איך מצטרפים?"
+            />
+            <ReCAPTCHA
+              ref={captchaRef}
+              size="invisible"
+              sitekey={process.env.RECAPTCA_KEY!}
+            />
+            <FormField
+              disabled={isLoading || isSuccess}
+              type="submit"
+              label={getSubmitMessage({ isLoading, isSuccess })}
+              skin={getButtonSkin({ isLoading, isSuccess })}
+            />
+          </form>
+          <div className={style.formStatusWrapper}>
+            {isError && (
+              <Alert
+                text="אוי לא, קרתה שגיאה... בואו ננסה שוב מאוחר יותר"
+                type={AlertType.ERROR}
+              />
+            )}
+          </div>
+        </div>
+      </section>
     </Page>
   );
+};
+
+const getButtonSkin = (args: { isLoading?: boolean; isSuccess?: boolean }) => {
+  if (args.isLoading) {
+    return "loading";
+  }
+  if (args.isSuccess) {
+    return "success";
+  }
+};
+
+const getSubmitMessage = (args: {
+  isLoading?: boolean;
+  isSuccess?: boolean;
+}) => {
+  if (args.isLoading) {
+    return "שולח...";
+  }
+  if (args.isSuccess) {
+    return "ההודעה נשלחה!";
+  }
 };
 
 export default JoinUsPage;
