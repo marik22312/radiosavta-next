6793ab118e428af4f4f78467c8912b97ced48e17
mark22312 <mark22312@gmail.com>
2023-04-29 14:17:29 +0300

    Refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
-import { useState, useRef, useEffect } from "react";
 import { Page } from "../components/ui/Page";
 import style from "./Home.module.scss";
 
 import cn from "classnames";
 
-import { useKeenSlider } from "keen-slider/react";
 import { GetServerSideProps } from "next";
 import { QueryClient } from "react-query";
 import { stringify } from "flatted";
 import { dehydrate } from "react-query/hydration";
-import { RecordedShowsListStandalone } from "../components/RecordedShowsList/RecordedShowsListStandalone";
 
-import Link from "next/link";
 import { usePrograms } from "../hook/usePrograms";
-import { ProgramsListStandalone } from "../components/ProgramsList/ProgramsListStandalone";
 import { getAllActivePrograms } from "../api/Programs.api";
 import {
   prefetchLatestRecordedShows,
@@ -21,17 +16,14 @@ import {
 } from "../hook/useLatestRecordedShows";
 
 import { getFilteredImages } from "../utils/getRandomImages.utils";
-import Image from "next/image";
 import { Heading } from "../components/ui/Typography";
 import { Colors } from "../components/ui/Colors";
-import { useAgenda } from "../hook/useAgenda";
+import { prefetchAgenda, useAgenda } from "../hook/useAgenda";
 import { usePlayerControls } from "../providers/PlayerProvider/usePlayerControls";
 import { programParser } from "../parsers/Programs.parser";
 import { usePlayerState } from "../providers/PlayerProvider/usePlayerState";
 
 export const Home: React.FC<{ imagesToShow: string[] }> = (props) => {
-  const { programs } = usePrograms({ limit: 3, rand: true });
-
   return (
     <Page title="ראשי">
       <AboutSection imagesToShow={props.imagesToShow} />
@@ -42,27 +34,6 @@ export const Home: React.FC<{ imagesToShow: string[] }> = (props) => {
 
 export const AboutSection: React.FC<{ imagesToShow: string[] }> = (props) => {
   const { data } = useAgenda();
-  const timer = useRef<any>();
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
-    loop: true,
-    duration: 1000,
-    slideChanged(s) {
-      setCurrentSlide(s.details().relativeSlide);
-    },
-  });
-
-  useEffect(() => {
-    timer.current = setInterval(() => {
-      if (slider) {
-        slider.next();
-      }
-    }, 7000);
-    return () => {
-      clearInterval(timer.current);
-    };
-  }, [slider]);
-
   return (
     <section>
       <div style={{ paddingRight: "21px" }}>
@@ -121,7 +92,7 @@ const UploadsSection: React.FC = () => {
                   },
                 })
               }
-              data-isPlaying={songTitle === show.name}
+              data-isplaying={songTitle === show.name}
             >
               <h4>{show.program?.name_he}</h4>
               <h5>{show.name}</h5>
@@ -139,50 +110,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const queryClient = new QueryClient();
 
   await prefetchLatestRecordedShows(queryClient, { limit: 3 });
-
-  await queryClient.prefetchQuery("active-programs", () =>
-    getAllActivePrograms({ limit: 3, rand: true })
-  );
-
-  const imagesToShow = getFilteredImages();
-
+  await prefetchAgenda(queryClient);
+  
   return {
     props: {
       dehydratedState: stringify(dehydrate(queryClient)),
-      imagesToShow,
     },
   };
-};
-
-interface GalleryArrowProps {
-  onClick: (e: any) => void;
-  disabled: boolean;
-}
-
-const ArrowLeft: React.FC<GalleryArrowProps> = (props) => {
-  const disabeld = props.disabled ? " arrowDisabled" : "";
-  return (
-    <svg
-      onClick={props.onClick}
-      className={cn(style.arrow, style.arrowLeft, disabeld)}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-    </svg>
-  );
-};
-
-const ArrowRight: React.FC<GalleryArrowProps> = (props) => {
-  const disabeld = props.disabled ? " arrowDisabled" : "";
-  return (
-    <svg
-      onClick={props.onClick}
-      className={cn(style.arrow, style.arrowRight, disabeld)}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-    </svg>
-  );
 };
