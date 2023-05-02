6a3fc1ff87c9e84c3fa3b246f367e2fcaca2f07d
mark22312 <mark22312@gmail.com>
2023-05-02 23:28:30 +0300

    wip

--- pages/lp.tsx
+++ pages/lp.tsx
@@ -1,3 +1,4 @@
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 
 const LandingPage = () => {
@@ -6,6 +7,14 @@ const LandingPage = () => {
 		<div style={{minHeight: '500px', backgroundColor: "#C3C3C3"}}><div><h2>LandingPage</h2></div></div>
 		</>
 	)
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+	console.log('#######', context.req.cookies);
+
+	return {
+		props: {}
+	}
 }
 
 export default LandingPage
