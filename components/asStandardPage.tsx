eb048539a38008b6a94f059c8f9eb94ccb30f1b8
asafhada <asafhada@checkpoint.com>
2023-05-09 18:39:23 +0300

    space between content and player on finish scroll

--- components/asStandardPage.tsx
+++ components/asStandardPage.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import PlayerWrapper from './FooterPlayer/PlayerWrapper/PlayerWrapper';
-import { Navbar } from './ui/Page';
+import React from "react";
+import PlayerWrapper from "./FooterPlayer/PlayerWrapper/PlayerWrapper";
+import { Navbar } from "./ui/Page";
 
 export const asStandardPage = (Page: React.ElementType) => {
-	// @ts-expect-error
-	Page.getLayout = (page) => {
-		return (
-			<>
-			<Navbar/>
-			<div>
-				{page}
-			</div>
-			<PlayerWrapper />
-			</>
-		)
-	}
+  // @ts-expect-error
+  Page.getLayout = (page) => {
+    return (
+      <>
+        <Navbar />
+        <div style={{ paddingBottom: 15 }}>{page}</div>
+        <PlayerWrapper />
+      </>
+    );
+  };
 
-	return Page;
-}
+  return Page;
+};
