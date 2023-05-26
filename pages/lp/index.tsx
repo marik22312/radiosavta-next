317a8d508f49fe00a7d4276d141c555709304578
mark22312 <mark22312@gmail.com>
2023-05-26 18:12:02 +0300

    Fixed LP

--- pages/lp/index.tsx
+++ pages/lp/index.tsx
@@ -5,9 +5,8 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import Play from "../components/PlayPauseButton/Button/Play.svg";
-import styles from "./lp/landingPage.module.scss";
-import { AudioPlayer } from '../components/FooterPlayer/AudioPlayer/AudioPlayer';
-import { useLivePlayer } from '../hook/useLivePlayer';
+import styles from "./LandingPage.module.scss";
+import { useLivePlayer } from '../../hook/useLivePlayer';
 
 const LandingPage = () => {
   const {toggleLive} = useLivePlayer();
